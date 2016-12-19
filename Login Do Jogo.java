6429956eeb7ead9004c9e598c030317839ad7144
MasaakiNakamura <masaaki_naka@hotmail.com>
2016-12-18 23:27:30 -0300

    Update Login Do Jogo.java

--- Login Do Jogo.java
+++ Login Do Jogo.java
@@ -1,34 +1,31 @@
 package prova;
 
 import javax.swing.JOptionPane;
-public class loginDoJogo {
-	LerTxt lertxt = new LerTxt();
-	CriarTxt criartxt = new CriarTxt();
-	String usuarioLogin;
-	String usuarioSenha;
-	int i; //variáveis que servem para definir a linha e a coluna da matriz de logins
+public class Login {
 	public void login(){
-
+		int i;
+		boolean controleDeLogin = true;
+		String nomeDeUsuario;
+		String senhaDeUsuario;
+		String usuarios[] = {"tsylasac@gmail.com", "fcfmuller@gmail.com", "projetonewton.ufpa@gmail.com",
+							"renatofarias@gmail.com", "arissaniwa3@gmail.com", "masaaki_naka@hotmail.com"};
+		String senhas[] = {"thiagosylas", "franciscomuller", "projetonewton", "renatofarias",
+							"arissaniwa", "masaakinakamura"};
+		JOptionPane.showMessageDialog(null, "Coloque seus dados de login");
 		
-		boolean controleDeLogin = false; //boolean para saber se o usuario acertou o errou o login da conta
-		while(true){
-			usuarioLogin = JOptionPane.showInputDialog("Nome de usuário:");
-			usuarioSenha = JOptionPane.showInputDialog("Senha:");
-			for(i = 0; i < criartxt.a; i++){
-				if(criartxt.login[i][0].equals(usuarioLogin)){
-					if(criartxt.login[i][0].equals(usuarioSenha)){
-						JOptionPane.showMessageDialog(null, "Dados corretos");
-						JOptionPane.showMessageDialog(null, "Bom Jogo!!");
-						controleDeLogin = true;
-						break;
-					}
+		while(controleDeLogin){
+			nomeDeUsuario = JOptionPane.showInputDialog("Digite seu nome de usuario ou email: \nDica: Coloque o seu email");
+			senhaDeUsuario = JOptionPane.showInputDialog("Digite sua senha: \nDica: Coloque seu nome");
+		
+			for(i = 0; i < 6; i++){
+				if((nomeDeUsuario.equals(usuarios[i]) && (senhaDeUsuario.equals(senhas[i])))){
+					JOptionPane.showMessageDialog(null, "Login feito com sucesso");
+					controleDeLogin = false;
 				}
-			}
+			}		
+		
 			if(controleDeLogin){
-				break;
-			} else{
-				JOptionPane.showMessageDialog(null, "Dados incorretos!!" + "\n" +
-						"Por favor, insira os dados de login, novamente:");
+				JOptionPane.showMessageDialog(null, "Dados de Login inválidos" + "\n" + "Insira os dados novamente:");
 			}
 		}
 	}
